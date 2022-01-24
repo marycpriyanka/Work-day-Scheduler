b27dbf99fa38a8ce8f84474d116183564190c3cb
Priyanka Mary Chrstine <marycpriyanka@gmail.com>
2022-01-24 12:36:33 -0800

    Sve events to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,35 @@
 let currentDayPElement = $("#currentDay");
+let container = $(".container");
 
+let currentDay = moment().format("dddd, DD MMM YY");
 // Stores the current hour of the day
-let currentTime = moment().format("kk");
+let currentHour = moment().format("HH");
+moment.duration(1,"hours");
+
+let eventArray =  JSON.parse(localStorage.getItem(currentDay)) || [];
+
+// Event listener for save button
+container.on("click", ".saveBtn", saveEvent);
 
 // Function will be called when page is loaded
 $(function () {
-    currentDayPElement.text(moment().format("dddd, DD MMM YY"));
+    currentDayPElement.text(currentDay);
     colorCodeTimeBlock();
+    displayEvents();
+    clearPreviousDayInLocalStorage();
+
+    let timer = setInterval(function() {
+        if (currentHour < moment().format("HH")) {
+            currentHour = moment().format("HH");
+            colorCodeTimeBlock();
+        }
+    },1000);
 });
 
 // Color codes time block indicating whether it is in past, present or future
 function colorCodeTimeBlock() {
     // The current hour is set as bg-warning, all future hours are set as bg-info
-    switch (currentTime) {
+    switch (currentHour) {
         case "1":
         case "2":
         case "3":
@@ -21,53 +38,82 @@ function colorCodeTimeBlock() {
         case "6":
         case "7":
         case "8":
-            $("#eight~.container .col-10").addClass("bg-info");
+            $("#eight~.row .col-10").addClass("future");
             break;
 
         case "9":
-            $("#nine .col-10").addClass("bg-warning");
-            $("#nine~.container .col-10").addClass("bg-info");
+            $("#nine .col-10").addClass("present");
+            $("#nine~.row .col-10").addClass("future");
             break;
 
         case "10":
-            $("#ten .col-10").addClass("bg-warning");
-            $("#ten~.container .col-10").addClass("bg-info");
+            $("#ten .col-10").addClass("present");
+            $("#ten~.row .col-10").addClass("future");
             break;
 
         case "11":
-            $("#eleven .col-10").addClass("bg-warning");
-            $("#eleven~.container .col-10").addClass("bg-info");
+            $("#eleven .col-10").addClass("present");
+            $("#eleven~.row .col-10").addClass("future");
             break;
 
         case "12":
-            $("#twelve .col-10").addClass("bg-warning");
-            $("#twelve~.container .col-10").addClass("bg-info");
+            $("#twelve .col-10").addClass("present");
+            $("#twelve~.row .col-10").addClass("future");
             break;
 
         case "13":
-            $("#thirteen .col-10").addClass("bg-warning");
-            $("#thirteen~.container .col-10").addClass("bg-info");
+            $("#thirteen .col-10").addClass("present");
+            $("#thirteen~.row .col-10").addClass("future");
             break;
 
         case "14":
-            $("#fourteen .col-10").addClass("bg-warning");
-            $("#fourteen~.container .col-10").addClass("bg-info");
+            $("#fourteen .col-10").addClass("present");
+            $("#fourteen~.row .col-10").addClass("future");
             break;
 
         case "15":
-            $("#fifteen .col-10").addClass("bg-warning");
-            $("#fifteen~.container .col-10").addClass("bg-info");
+            $("#fifteen .col-10").addClass("present");
+            $("#fifteen~.row .col-10").addClass("future");
             break;
 
         case "16":
-            $("#sixteen .col-10").addClass("bg-warning");
-            $("#sixteen~.container .col-10").addClass("bg-info");
+            $("#sixteen .col-10").addClass("present");
+            $("#sixteen~.row .col-10").addClass("future");
             break;
 
         case "17":
-            $("#seventeen .col-10").addClass("bg-warning");
-            $("#seventeen~.container .col-10").addClass("bg-info");
+            $("#seventeen .col-10").addClass("present");
             break;
     }
+}
 
+// Displays the events saved in local storage 
+function displayEvents() {
+    for (let i = 0; i < eventArray.length; i++) {
+        let timeBlock = $("#" + eventArray[i].hour);
+        timeBlock.children("textarea").eq(0).val(eventArray[i].plan);
+    }
+}
+
+// Clears all previous day entries in local storage
+function clearPreviousDayInLocalStorage() {
+    for (let i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== currentDay) {
+            localStorage.removeItem(localStorage.key(i));
+        }
+    }
+}
+
+// Event is stored in local storage
+function saveEvent(event) {
+    let hour = $(event.target).parent().attr("id");
+    let plan = $(event.target).siblings("textarea").val();
+
+    let eventObject = {
+        hour: hour,
+        plan: plan
+    };
+
+    eventArray.push(eventObject);
+    localStorage.setItem(currentDay, JSON.stringify(eventArray));
 }
